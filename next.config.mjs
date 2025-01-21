24ca703cbdab6df613a2bdfaf02679f82ec32863
MidNight-04 <vksraghav4@gmail.com>
2025-01-21 12:21:54 +0530

    Host Change

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    // REACT_APP_BASE_PATH: "http://localhost:8080",
-    REACT_APP_BASE_PATH: "http://192.168.1.12:8080",
+    REACT_APP_BASE_PATH: "http://3.6.218.211:8080",
+    // REACT_APP_BASE_PATH: "http://192.168.1.12:8080",
     NEXTAUTH_SECRET: "QAHzHrsHgYUL3hJcE/RprVVK2vxVyalv/LjsZl11n8U=",
   },
   images: {

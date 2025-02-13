94c39764ce96c2eab4acf0a61ff651be1f5a9ac6
Vikas Raghav <97186993+MidNight-04@users.noreply.github.com>
2025-02-13 11:46:51 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     // REACT_APP_BASE_PATH: "http://3.6.218.211:8080",
-    REACT_APP_BASE_PATH: "http://3.110.224.108:8080",
-    // REACT_APP_BASE_PATH: "http://localhost:8080",
+    // REACT_APP_BASE_PATH: "http://3.110.224.108:8080",
+    REACT_APP_BASE_PATH: "http://localhost:8080",
     // REACT_APP_BASE_PATH: "https://server-1-5nta.onrender.com",
     NEXTAUTH_SECRET: "QAHzHrsHgYUL3hJcE/RprVVK2vxVyalv/LjsZl11n8U=",
   },
